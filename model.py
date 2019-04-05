f489dc9c81147e1f7cf113b39845a0cf8e3c0154
Dhananjai Sharma <dhananjaisharma26@gmail.com>
2019-04-04 23:15:04 -0400

    first push

---
+++ model.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    def __init__(self, params):
+        super(Generator, self).__init__()
+
+        
+
+    def forward(self, *input):
+        return super().forward(*input)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, params):
+        super(Discriminator, self).__init__()
+
+
+    def forward(self, *input):
+        return super().forward(*input)
